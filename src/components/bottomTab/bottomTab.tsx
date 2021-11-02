ba75c2a0b21a5b3d20a13a301a4f304c749e4bae
Vinicius Paulino da Silva <viniciussilvapaulino0@gmail.com>
2021-11-02 18:36:31 -0300

    fix: global css

---
+++ src/components/bottomTab/bottomTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import styles from "./bottomTab.styles";
+
+const BottomTab: React.FC = () => {
+  return (
+    <div>
+      <h2>hi</h2>
+    </div>
+  );
+};
+
+export default BottomTab;
